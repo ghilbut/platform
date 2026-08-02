09bbe4866c6cff1f7649f10cb902b3592973005b
ghilbut <ghilbut@gmail.com>
2026-08-02 23:37:33 +0900

    chore(identity): add default AWS tags

--- aws/foundation/identity/tofu/providers.tf
+++ aws/foundation/identity/tofu/providers.tf
@@ -1,4 +1,17 @@
 provider "aws" {
   profile = "ghilbut"
   region  = "us-east-1"
+
+  default_tags {
+    tags = {
+      created_by      = "opentofu"
+      managed_by      = "opentofu"
+      org             = "ghilbut"
+      project         = "platform"
+      service         = "identity"
+      component       = "aws"
+      "opentofu/repo" = "https://github.com/ghilbut/platform"
+      "opentofu/path" = "aws/foundation/identity/tofu/"
+    }
+  }
 }
