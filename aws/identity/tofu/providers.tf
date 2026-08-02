23d1cbd041ea667ff406c4abd59eb3fef29625eb
ghilbut <ghilbut@gmail.com>
2026-08-02 11:25:45 +0900

    refactor(identity): move Organizations state backend

--- aws/identity/tofu/providers.tf
+++ aws/identity/tofu/providers.tf
@@ -8,10 +8,10 @@ provider "aws" {
       managed_by      = "opentofu"
       org             = "ghilbut"
       project         = "platform"
-      service         = "accounts"
+      service         = "identity"
       component       = "aws"
       "opentofu/repo" = "https://github.com/ghilbut/platform"
-      "opentofu/path" = "accounts/tofu/"
+      "opentofu/path" = "aws/identity/tofu/"
     }
   }
 }
