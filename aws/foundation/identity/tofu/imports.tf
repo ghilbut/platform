842290ea0e6243f258a92dba4f73baa6c58be4cd
ghilbut <ghilbut@gmail.com>
2026-08-04 01:53:26 +0900

    chore(foundation): remove completed imports

--- aws/foundation/identity/tofu/imports.tf
+++
@@ -1,9 +0,0 @@
-import {
-  to = aws_identitystore_group.devops
-  id = "d-906671970d/94183498-5041-705e-ddc0-aa6c2e714fbc"
-}
-
-import {
-  to = aws_identitystore_group_membership.devops_ghilbut
-  id = "d-906671970d/d4681438-c011-703c-628d-3a176b3f7b1f"
-}
