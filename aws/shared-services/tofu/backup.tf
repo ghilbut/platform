3ae4c74026fabbd15661757eeb86234d49ab7db6
ghilbut <ghilbut@gmail.com>
2026-08-08 16:57:44 +0900

    feat(vault): add AWS unseal and backup access

--- aws/shared-services/tofu/backup.tf
+++ aws/shared-services/tofu/backup.tf
@@ -1,5 +1,7 @@
 locals {
-  cpa_snapshot_prefix = "k3s/cpa"
+  cpa_snapshot_prefix                  = "k3s/cpa"
+  vault_backup_prefix                  = "vault/cpa/raft"
+  vault_backup_service_account_subject = "system:serviceaccount:vault:vault-backup"
 }
 
 resource "aws_s3_bucket" "backups" {
@@ -149,4 +151,84 @@ resource "aws_iam_user_policy" "cpa_snapshot" {
   name   = "k3s-cpa-snapshot"
   user   = aws_iam_user.cpa_snapshot.name
   policy = data.aws_iam_policy_document.cpa_snapshot.json
+}
+
+data "aws_iam_policy_document" "vault_backup_assume" {
+  statement {
+    sid     = "AllowCpaVaultBackupServiceAccount"
+    effect  = "Allow"
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.cpa.arn]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "${trimprefix(local.cpa_oidc_issuer, "https://")}:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "${trimprefix(local.cpa_oidc_issuer, "https://")}:sub"
+      values   = [local.vault_backup_service_account_subject]
+    }
+  }
+}
+
+resource "aws_iam_role" "vault_backup" {
+  name                 = "vault-cpa-backup"
+  description          = "CPA Vault Raft snapshot writer for the platform backup bucket."
+  max_session_duration = 3600
+  assume_role_policy   = data.aws_iam_policy_document.vault_backup_assume.json
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+data "aws_iam_policy_document" "vault_backup" {
+  statement {
+    sid       = "ReadBackupBucketLocation"
+    effect    = "Allow"
+    actions   = ["s3:GetBucketLocation"]
+    resources = [aws_s3_bucket.backups.arn]
+  }
+
+  statement {
+    sid       = "ListVaultBackups"
+    effect    = "Allow"
+    actions   = ["s3:ListBucket"]
+    resources = [aws_s3_bucket.backups.arn]
+
+    condition {
+      test     = "StringLike"
+      variable = "s3:prefix"
+      values = [
+        local.vault_backup_prefix,
+        "${local.vault_backup_prefix}/*",
+      ]
+    }
+  }
+
+  statement {
+    sid    = "ManageVaultBackups"
+    effect = "Allow"
+    actions = [
+      "s3:AbortMultipartUpload",
+      "s3:DeleteObject",
+      "s3:GetObject",
+      "s3:ListMultipartUploadParts",
+      "s3:PutObject",
+    ]
+    resources = ["${aws_s3_bucket.backups.arn}/${local.vault_backup_prefix}/*"]
+  }
+}
+
+resource "aws_iam_role_policy" "vault_backup" {
+  name   = "vault-cpa-backup"
+  role   = aws_iam_role.vault_backup.name
+  policy = data.aws_iam_policy_document.vault_backup.json
 }
